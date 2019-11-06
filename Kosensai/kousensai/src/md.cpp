7e83ce4592905569746ab620ce2202e88a6c9439
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-11-06 22:23:52 +0000

    change pigpio

--- Kosensai/kousensai/src/md.cpp
+++ Kosensai/kousensai/src/md.cpp
@@ -12,11 +12,10 @@ constexpr int wheel_1_pin[3] = {10, 9, 11};
 constexpr int wheel_2_pin[3] = {5, 6, 13};
 constexpr int wheel_3_pin[3] = {25, 8, 7};
 
-int pwm_right_front, pwm_left_front, pwm_right_back, pwm_left_back;
+int gpio_handle, pwm_right_front, pwm_left_front, pwm_right_back, pwm_left_back;
 
 class l298n {
 private:
-  int gpio_handle;
   int pwm_pin, change_pin1, change_pin2;
   void front(int pwm);
   void back(int pwm);
@@ -30,7 +29,6 @@ l298n::l298n(int user_pwm_pin, int user_change_pin1, int user_change_pin2) {
   pwm_pin = user_pwm_pin;
   change_pin1 = user_change_pin1;
   change_pin2 = user_change_pin2;
-  gpio_handle = pigpio_start(0, 0);
   set_mode(gpio_handle, change_pin1, PI_OUTPUT);
   set_mode(gpio_handle, change_pin2, PI_OUTPUT);
   set_mode(gpio_handle, pwm_pin, PI_OUTPUT);
@@ -59,6 +57,7 @@ void mdCallback(const std_msgs::Int16MultiArray &msg) {
   pwm_left_back = msg.data[3];
 }
 int main(int argc, char **argv) {
+  gpio_handle = pigpio_start(0, 0);
   cout << "start" << endl;
   ros::init(argc, argv, "md_info");
   ros::NodeHandle n;
